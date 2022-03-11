ee56620be08cb9f2d38acd0627aaa0efd0a46fe7
Josh Kellhofer <sasukekorlo@yahoo.com>
2022-03-11 12:15:27 -0800

    Deleted Unused Files + Created Table in Apps.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+
+const [entries, setEntries] = useState([{id: 1, Title: 'new', Artist: 'You', Album: 'Lovin It', Release_Date: '01-01-2001', Genre: 'Pop'}])
+
+// const [title, setTitle] = useState([{}]) //state variables for form
+// const [artist, setArtist] = useState([]) //state variables for form
+// const [album, setAlbum] = useState([]) //state variables for form
+// const [releasedate, setReleaseDate] = useState([]) //state variables for form
+// const [genre, setGenre] = useState([]) //state variables for form
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <table>
+       <thead>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Artist</th>
+          <th>Album</th>
+          <th>Release Date</th>
+          <th>Genre</th>
+        </tr>
+       </thead>
+       <tbody>
+         <tr>
+           <td>1</td>
+           <td>1</td>
+           <td>1</td>
+           <td>1</td>
+           <td>1</td>
+           <td>1</td>
+         </tr>
+       </tbody>
+     </table>
     </div>
   );
 }
+        
 
 export default App;
